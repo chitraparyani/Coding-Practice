fb46ef269eeba2f2402a78ab87a66d9916cb328e
Chitra Paryani <paryani.c@husky.neu.edu>
2019-07-21 21:23:54 -0400

    Update MergeTwoSortedList.java

--- MergeTwoSortedList.java
+++ MergeTwoSortedList.java
@@ -43,4 +43,24 @@ class MergeTwoSortedList {
         }
         return result.next;
     }
+	
+	/** Second approach using recursion **/
+	
+	    public ListNode mergeTwoListsI(ListNode l1, ListNode l2) {
+        
+        if(l1 == null)
+            return l2;
+        
+        if(l2 == null)
+            return l1;
+        
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else{
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
 }
